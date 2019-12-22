17d9f0be44727a50534cfc9292d6b49dfac6cba4
Justis Gipson <justis710@gmail.com>
2019-12-22 14:25:09 -0500

    update to keep app awake function

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import http from 'http';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@material/icon-button';
 
-setInterval(function() {
+setInterval(() => {
     http.get('http://jg-gear-locker.herokuapp.com')
 }, 30000) // wake every 5 minutes
 
