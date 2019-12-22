97cb0a7d71e028a4557f53cda6338948212b3867
Justis Gipson <justis710@gmail.com>
2019-12-22 13:49:37 -0500

    added http process to keep app awake

--- src/index.js
+++ src/index.js
@@ -2,8 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-
+import http from 'http';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@material/icon-button';
+
+setInterval(function() {
+    http.get('http://jg-gear-locker.herokuapp.com')
+}, 30000) // wake every 5 minutes
 
 ReactDOM.render(<App />, document.getElementById('root'));
